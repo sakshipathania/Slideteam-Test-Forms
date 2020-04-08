1369fb18ffa929656d8fd9ee351b34ad4cf37bf2
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-04-08 12:38:00 +0530

    Update ResearchServicesSteps.java

--- src/test/java/SetupClass/TestStep/ResearchServicesSteps.java
+++ src/test/java/SetupClass/TestStep/ResearchServicesSteps.java
@@ -7,6 +7,7 @@ import java.sql.Date;
 import java.text.SimpleDateFormat;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 
 import SetupClass.Setup;
 import cucumber.api.java.en.Given;
@@ -67,9 +68,13 @@ public class ResearchServicesSteps extends Setup {
 
 	@Then("^user click on submit button on rs form$")
 	public void user_click_on_submit_button_on_rs_form() throws Throwable {
+		try {
 		Thread.sleep(1000);
 		driver.findElement(By.id("finalSubmit")).submit();
 		Thread.sleep(1000);
+		}
+		catch (NoSuchElementException popup) {
+	}
 		/*
 		 * System.out.print("form is submitted at:->> ");
 		 * //System.out.println(System.currentTimeMillis()/1000); SimpleDateFormat
