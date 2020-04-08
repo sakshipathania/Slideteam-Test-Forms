8129e986219bc38794567fbcbb925993880e5d67
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-04-08 17:14:36 +0530

    Update BlogComment.java

--- src/test/java/SetupClass/TestStep/BlogComment.java
+++ src/test/java/SetupClass/TestStep/BlogComment.java
@@ -50,30 +50,21 @@ public class BlogComment extends Setup {
 		 ((Alert) confirmBox).dismiss();
 		Thread.sleep(1000);
 	}
-	
-	@Then("^user goes to leave a comment$")
-	public void user_goes_to_leave_a_comment() throws Throwable {
-		
-         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	
-		
-		driver.findElement(By.xpath("//*[@id='respond']/fieldset"));
-		Thread.sleep(2000);
-	}					    
+						    
 			
-	@Then("^user enter name on blog form$") 
-	public void user_enter_name_on_blog_form() throws Throwable {
+	//* @Then("^user enter name on blog form$") 
+	/* public void user_enter_name_on_blog_form() throws Throwable {
 		Thread.sleep(1000);
 		driver.findElement(By.id("author")).sendKeys("SlideTeam Testing");
 		Thread.sleep(1000);
-	}
+	} 
 	
 	@Then("^user enter email on blog form$")
 	public void user_enter_email_on_blog_form() throws Throwable {
 		Thread.sleep(1000);
 		driver.findElement(By.id("email")).sendKeys("Sakshi.pathania@slidetech.in");
 		Thread.sleep(1000);
-	}	
+	}	*/
 	
 	@Then("^user enter comment on blog form$")
 	public void user_enter_comment_on_blog_form() throws Throwable {
