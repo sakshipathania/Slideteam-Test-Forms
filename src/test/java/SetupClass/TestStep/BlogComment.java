c42f02d0c8798041bb1571c621438efde6f704e9
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-04-09 18:04:37 +0530

    Update BlogComment.java

--- src/test/java/SetupClass/TestStep/BlogComment.java
+++ src/test/java/SetupClass/TestStep/BlogComment.java
@@ -36,10 +36,15 @@ public class BlogComment extends Setup {
 		Thread.sleep(3000);
 		driver.manage().deleteAllCookies();
 		   Thread.sleep(2000);
-	          driver.findElement(By.cssSelector("#blog-detail-popup > div > div > div.modal-header > button")).click();
-		Thread.sleep(1000);
-		
+	          
 	}
+	
+	@Then("^user close the coupon popup$")
+    public void user_close_the_coupon_popup() throws Throwable {
+        
+        driver.findElement(By.cssSelector("#blog-detail-popup > div > div > div.modal-header > button")).click();
+		Thread.sleep(1000);
+    }
 			    			    
 	@Then("^user enter comment on blog form$")
 	public void user_enter_comment_on_blog_form() throws Throwable {
